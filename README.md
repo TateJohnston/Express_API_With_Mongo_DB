3ddebbc1e6ee015342844e789f9c368082d2de31
TateJohnston <tate1johnston@gmail.com>
2025-05-12 21:37:40 +0800

    first commit of lab 9 on Monday 12th May

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Express_API_With_Mongo_DB
+
 Labs for our 9th module, dealing with hooking up an api and a mongo database.
+
+# important step
+
+User needs to create their own .env file that link to their database and the port they intend to run it on. This file should sit at the root of the project.
---
+++ mondogb-app/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+let dbConnect = require("./dbConnect");
+let userRoutes = require("./routes/userRoutes");
+require("dotenv").config();
+
+app.use(express.json());
+
+app.use("/api/users", userRoutes);
+
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to my MongoDB application." });
+});
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}.`);
+});
